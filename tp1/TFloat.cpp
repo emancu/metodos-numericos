3e5a83e579d8ee60202330b2307de992e5781cdf
tonchis <lucas.tolchinsky@gmail.com>
2012-03-30 19:39:27 -0300

    Adds TFloat.

---
+++ tp1/TFloat.cpp
@@ -0,0 +1,109 @@
+#include "TFloat.h"
+#include <math.h>
+
+TFloat::TFloat(size_t t)
+{
+	_valor = 0.0;
+	set_precision(t);
+	recortar();
+}
+
+TFloat::TFloat(int i, size_t t)
+{
+	_valor = (double) i;
+	set_precision(t);
+	recortar();
+}
+
+TFloat::TFloat(float f, size_t t)
+{
+	_valor = (double) f;
+	set_precision(t);
+	recortar();
+}
+
+TFloat::TFloat(double f, size_t t)
+{
+	_valor = f;
+	set_precision(t);
+	recortar();
+}
+
+double TFloat::dbl() const
+{
+	return _valor;
+}
+
+void  TFloat::operator =(const TFloat& f)
+{
+	_valor = f._valor;
+	_t = f._t;
+}
+
+bool  TFloat::operator ==(const TFloat& f) const
+{
+	return (_valor == f._valor);
+}
+
+TFloat TFloat::operator + (const TFloat& f) const
+{
+	double result = _valor + f._valor;
+    return TFloat(result, _t);
+}
+
+TFloat TFloat::operator - (const TFloat& f) const
+{
+	double result = _valor - f._valor;
+    return TFloat(result, _t);
+}
+
+TFloat TFloat::operator * (const TFloat& f) const
+{
+	double result = _valor * f._valor;
+    return TFloat(result, _t);
+}
+
+TFloat TFloat::operator / (const TFloat& f) const
+{
+	double result = _valor / f._valor;
+    return TFloat(result, _t);
+}
+
+TFloat TFloat::exponencial() const
+{
+	double result = exp(_valor);
+    return TFloat(result, _t);
+}
+
+TFloat TFloat::operator+ (const double& f) const
+{
+	return this->operator+(TFloat(f,_t));
+}
+
+TFloat TFloat::operator- (const double& f) const
+{
+	return this->operator-(TFloat(f,_t));
+}
+
+TFloat TFloat::operator* (const double& f) const
+{
+	return this->operator*(TFloat(f,_t));
+}
+
+TFloat TFloat::operator/(const double& f) const
+{
+	return this->operator/(TFloat(f,_t));
+}
+
+
+
+void TFloat::recortar()
+{
+	bitset<64> * bits;
+	bits = (bitset<64>*) &_valor;
+
+	for(size_t i = 0 ; i < (52 - _t); i++ )
+	{
+		(*bits)[i] = 0;
+	}
+}
---
+++ tp1/TFloat.h
@@ -0,0 +1,88 @@
+/******************************************************
+ * Clase TFloat - Versión 1.00
+ *
+ * La clase TFloat encapsula el comportamiento de una
+ * aritmética finita de punto flotante que utiliza una
+ * cantidad arbitraria de bits en la representación de
+ * la mantisa.
+ *
+ * La clase está basada en el tipo de datos double de C++
+ * y por lo tanto la máxima precisión para la mantisa está
+ * limitada a 52 bits.
+ *
+ * Esta clase fue desarrollada por el grupo de docentes del
+ * laboratorio de Métodos Numéricos (DC-FCEyN-UBA) con el
+ * objetivo de que sea utilizada por los alumnos de esta
+ * materia en el desarrollo de los trabajos prácticos de la
+ * misma.
+ *
+ * Esta implementación puede contener errores. En caso de
+ * encontrar algún error, se agradecerá que el mismo sea
+ * informado a metnum-labo@dc.uba.ar para que pueda ser
+ * corregido en las futuras versiones.
+ *
+ *****************************************************/
+
+#ifndef __TFLOAT_H__
+#define __TFLOAT_H__
+
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+class TFloat
+{
+  public:
+	/**
+	 * Constructores y destructor
+	 * El parámetro t en cada constructor indica la cantidad
+	 * de bits de mantisa que se desean usar en el TFloat
+	 */
+	TFloat(size_t t);
+	TFloat(double f, size_t t);
+	TFloat(float f, size_t t);
+	TFloat(int i, size_t t);
+	~TFloat(){}
+
+	/**
+	 * Los siguientes sirven para setear y obtener la precisión
+	 * utilizada por el TFloat
+	 */
+	size_t precision() { return _t; } const
+	void set_precision(const size_t t) { _t = (t <= 52? t : 52); }
+
+	// Operadores
+	void operator=(const TFloat& );
+	bool operator==(const TFloat& ) const;
+	TFloat operator+(const TFloat&) const;
+	TFloat operator-(const TFloat&) const;
+	TFloat operator*(const TFloat&) const;
+	TFloat operator/(const TFloat&) const;
+
+	// Operadores (que reciben un tipo primitivo)
+	TFloat operator+(const double&) const;
+	TFloat operator-(const double&) const;
+	TFloat operator*(const double&) const;
+	TFloat operator/(const double&) const;
+
+	/**
+	 * La siguiente función calcula el valor del número e
+	 * elevado al valor del TFloat
+	 */
+	TFloat exponencial() const;
+
+	/**
+	 * La siguiente función devuelve el valor del TFloat en un
+	 * double. Esta función sirve por ejemplo para imprimir por
+	 * pantalla el valor del TFloat.
+	 */
+	double dbl() const;
+
+private:
+	size_t _t;
+	double _valor;
+	void recortar();
+};
+
+#endif //__TFLOAT_H__
