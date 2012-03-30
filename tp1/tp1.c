e45cc97a9bac0267e0b35cdf98b50e4ad6c0e2d8
tonchis <lucas.tolchinsky@gmail.com>
2012-03-30 19:30:21 -0300

    Starts implementation of options parsing.

--- tp1/tp1.c
+++ tp1/tp1.c
@@ -1,17 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <unistd.h>
 
 #define GRAVITY 9.81
 
-void newton(char**);
-void bisection(char**);
+typedef struct params {
+  // General params
+  double h;
+  double v;
+  double tolerance;
+  double mass;
+  double cr;
+  int max_iterations;
+
+  // Bisection params
+  double a;
+  double b;
+
+  // Newton params
+  double x;
+} params;
+
+void newton(params);
+void bisection(params);
 double speed(double, double);
 double position(double, double, double);
 bool stopping_criteria(double, double, double);
 
-void bisection_with_friction(char**);
-void newton_with_friction(char**);
+void bisection_with_friction(params);
+void newton_with_friction(params);
 double position_with_friction(double h, double v, double time, double alpha);
 double speed_with_friction(double, double, double);
 
@@ -20,27 +38,76 @@ double speed_with_friction(double, double, double);
  *  Main
  */
 
+int main(int argc, char* argv[]){
+  // Aca se asignan los params parseados del CLI al estilo -h <valor> -v <valor>
+  static const char *optString = "Mhvtmciabx:";
+  int method;
+  params p;
+  int c;
+  while(c = getopt(argc, argv, optString) != -1){
+    switch(c){
+      case 'M':
+        method = atoi(optarg);
+        break;
+      case 'h':
+        p.h = atof(optarg);
+        break;
+      case 'v':
+        p.v = atof(optarg);
+        break;
+      case 't':
+        p.tolerance = atof(optarg);
+        break;
+      case 'm':
+        p.mass = atof(optarg);
+        break;
+      case 'c':
+        p.cr = atof(optarg);
+        break;
+      case 'i':
+        p.max_iterations = atoi(optarg);
+        break;
+      case 'a':
+        p.a = atof(optarg);
+        break;
+      case 'b':
+        p.b = atof(optarg);
+        break;
+      case 'x':
+        p.x = atof(optarg);
+        break;
+      default:
+        printf("no te parseo un carajo");
+    }
+  }
 
-int main(int r, char* argv[]){
-
-  int method = atoi(argv[1]);
+  // int method            = atoi(argv[1]);
+  // p.h              = atof(argv[2]);
+  // p.v              = atof(argv[3]);
+  // p.tolerance      = atof(argv[4]);
+  // p.mass           = atof(argv[5]);
+  // p.cr             = atof(argv[6]);
+  // p.max_iterations = atoi(argv[7]);
+  // p.a              = atof(argv[8]);
+  // p.b              = atof(argv[9]);
+  // p.x              = atof(argv[10]);
 
   switch(method) {
     case 0:
       printf("Bisection: \n");
-      bisection(argv);
+      bisection(p);
       break;
     case 1:
       printf("Bisection with friction: \n");
-      bisection_with_friction(argv);
+      bisection_with_friction(p);
       break;
     case 2:
       printf("Newton: \n");
-      newton(argv);
+      newton(p);
       break;
     case 3:
       printf("Newton with friction: \n");
-      newton_with_friction(argv);
+      newton_with_friction(p);
       break;
     default:
       printf("Metodo no valido.\n");
@@ -55,26 +122,26 @@ int main(int r, char* argv[]){
  */
 
 
-void bisection(char* argv[]) {
+void bisection(params p) {
   double m;
 
-  double h  = atof(argv[2]);
-  double vi = atof(argv[3]);
-  double a  = atof(argv[4]);
-  double b  = atof(argv[5]);
-  double tolerance  = atof(argv[6]);
-  int iteracion = atoi(argv[7]);
+  //double h  = atof(argv[2]);
+  //double vi = atof(argv[3]);
+  //double a  = atof(argv[4]);
+  //double b  = atof(argv[5]);
+  //double tolerance  = atof(argv[6]);
+  int iteracion = p.max_iterations;
 
-  if (position(h, vi, a)*position(h, vi, b) > 0){
+  if (position(p.h, p.v, p.a)*position(p.h, p.v, p.b) > 0){
     printf("Los bordes del intervalo no cumplen las condiciones.\n");
     // assert(true);
   }
 
 
-  while( --iteracion > 0 && !stopping_criteria(a,b, tolerance)) {
-    m = (b+a)/2;
+  while( --iteracion > 0 && !stopping_criteria(p.a,p.b, p.tolerance)) {
+    m = (p.b+p.a)/2;
 
-    if( position(h,vi,a) * position(h,vi,m) > 0 )
+    if( position(p.h,p.v,p.a) * position(p.h,p.v,m) > 0 )
       a = m;
     else
       b = m;
@@ -84,7 +151,7 @@ void bisection(char* argv[]) {
 }
 
 
-void newton(char* argv[]){
+void newton(params p){
   double h, v, previous, current, tolerance;
   int max_iterations, i;
   h = atof(argv[2]);
@@ -99,12 +166,11 @@ void newton(char* argv[]){
     current = previous - (position(h, v, previous)/speed(v, previous));
   }
 
-  // printf("%f\n", current);
   double m = current;
   printf("El valor encontrado es %lf en la position %lf a velocidad %lf \n", m, position(h,v,m), speed(v,m));
 }
 
-void newton_with_friction(char* argv[]){
+void newton_with_friction(params p){
   double h, v, previous, current, tolerance, mass, cr;
   int max_iterations, i;
   h = atof(argv[2]);
@@ -123,12 +189,11 @@ void newton_with_friction(char* argv[]){
     current = previous - (position_with_friction(h, v, previous, alpha)/speed_with_friction(v, previous, alpha));
   }
 
-  // printf("%f\n", current);
   double m = current;
   printf("El valor encontrado es %lf en la position %lf a velocidad %lf \n", m, position_with_friction(h,v,m, alpha), speed_with_friction(v,m, alpha));
 }
 
-void bisection_with_friction(char* argv[]) {
+void bisection_with_friction(params p) {
   double m;
 
   double h  = atof(argv[2]);
