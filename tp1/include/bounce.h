5db5d4ac55e066c7029c0d6c8f908e18c9229c69
tonchis <lucas.tolchinsky@gmail.com>
2012-04-05 15:57:31 -0300

    Adds bounce.

---
+++ tp1/include/bounce.h
@@ -0,0 +1,14 @@
+#ifndef __BOUNCE__
+#define __BOUNCE__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <tipos.h>
+#include <tools.h>
+
+void bounce(Params*);
+void bounce_with_friction(Params*);
+
+#endif
---
+++ tp1/src/bounce.c
@@ -0,0 +1,9 @@
+#include <bounce.h>
+
+void bounce(Params* p){
+
+}
+
+void bounce_with_friction(Params* p){
+
+}
