f838ab1fee3fff5838dddc3e471666b25dbfe377
tonchis <lucas.tolchinsky@gmail.com>
2012-05-05 19:35:33 -0300

    Building matrix.

--- tp2/tp2.c
+++ tp2/tp2.c
@@ -3,73 +3,86 @@
 #include <iostream>
 #include <stdlib.h>
 #include <sstream>
-
+#include <map>
 
 using namespace std;
 
-void binaryImage();
+void buildMatrix(double);
 
 /*
  *  Main
  */
 
 int main(int argc, char* argv[]){
-  binaryImage();
+  static const char *optString = "l:";
+  int c;
+  double lambda;
+
+  while((c = getopt(argc, argv, optString)) != -1){
+    switch(c){
+      case 'l': { lambda = atof(optarg); break; }
+      default:  { printf("Cannot parse.\n"); }
+    }
+  }
+
+  if(lambda == NULL) lambda = 1;
+  buildMatrix(lambda);
 
   return 0;
 }
 
-void binaryImage(){
-  printf("holaaaa \n");
+void buildMatrix(double lambda){
+  FILE* file = fopen("imagenes/64x64/blond.pgm", "r+b");
 
-  FILE* file = fopen ("imagenes/128x128/amber.pgm", "r+b");
-  FILE* output = fopen ("testA.pgm", "w");
-
+  // buffers
   char name[256];
   char number[256];
-
-
   int i,j ;
+
   for(i = 0; i <= 1 ; i++){
     fgets(name, 256, file);
-    printf("i = %d  = %s" , i, name);
-    fputs(name, output);
   }
 
-  int widthInt,heightInt,max;
-  fscanf (file, "%d", &widthInt);
-  fscanf (file, "%d", &heightInt);
+  int width, height, max;
+  fscanf (file, "%d", &width);
+  fscanf (file, "%d", &height);
   fscanf (file, "%d", &max);
-  printf("width = %d \nheight = %d \nmax = %d \n" , widthInt, heightInt, max);
 
-  sprintf(number, "%i %i\n" , widthInt, heightInt);
-  fputs(number, output);
+  sprintf(number, "%i %i\n", width, height);
 
   sprintf(number, "%i" , max);
-  fputs(number, output);
 
   //leo el salto de linea
   fread(name,1, 1, file);
-  fwrite(name,1,1,output);
 
-  for(i = 0; i <=256 ; i++){
+  for(i = 0; i <= 256; i++){
     name[i] = 0x0;
     number[i] = 0x0;
   }
 
-  for(i = 0; i < heightInt ; i++){
-     for(j=0; j < widthInt;j++){
-        if( i == j ){
-          name[0] = 0;
-          fwrite(name,1,1,output);
-          fread(name,1, 1, file);
-        } else {
-          fread(name,1, 1, file);
-          printf ("%d  ", (unsigned char) name[0]);
-          fwrite(name,1,1,output);
-        }
-     }
-    printf("\n\n");
-  }
+  map<int, map<int, int> > matrix;
+  int color;
+  int element = 0;
 
+  for(i = 0; i < height; i++){
+    for(j = 0; j < width; j++){
+      map<int, int> row;
+      fread(name, 1, 1, file);
+      color = (int) name[0];
+      row[-1] = color;
+
+      if(i == 0 || i == height - 1 || j == 0 || j == width - 1){
+        row[element] = 1;
+      }else{
+        row[element - 4] = -1;
+        row[element - 1] = -1;
+        row[element]     = lambda + 4;
+        row[element + 1] = -1;
+        row[element + 4] = -1;
+      }
+
+      matrix[element] = row;
+      element++;
+    }
+  }
 }
