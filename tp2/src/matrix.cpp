dd824d33d7786b02771f92a2c453af3019827d11
Alejandro Mataloni <amataloni@gmail.com>
2012-05-14 00:28:22 -0300

    improve gauss.. code not clean

--- tp2/src/matrix.cpp
+++ tp2/src/matrix.cpp
@@ -43,33 +43,71 @@ Matrix build_matrix(double lambda, PGMInfo* pgm_info){
 }
 
 void gauss(Matrix matrix, PGMInfo* pgm_info){
+  //ver el tema de hasta donde hacerlo...
+  //for(int column_number = 1; column_number < pgm_info->height * pgm_info->width; column_number++){
   for(int column_number = 1; column_number < pgm_info->height * pgm_info->width; column_number++){
-    for(int row_number = column_number + 1; row_number < row_number + pgm_info->width; row_number++){
+    printf("columnNUM = %d =>\n" , column_number);
+    for(int row_number = column_number + 1; row_number <= column_number + pgm_info->width && row_number < pgm_info->width * pgm_info->height; row_number++){
+      printf("   rowNumber = %d \n" , row_number);
       int index = column_number - (row_number - pgm_info->width);
-      if(matrix[row_number][index] != 0.0)
+      printf("   index = %d \n" , index );
+      if(matrix[row_number][index] != 0.0){
         substract_rows(matrix, pgm_info, row_number, column_number, index);
+      }
     }
   }
 }
 
 // column_number is the same row that will be used to substract.
 void substract_rows(Matrix matrix, PGMInfo* pgm_info, int row_number, int column_number, int index){
+
+  for(int i = 0; i < 16; i++){
+    for(int j = 0; j < 2 * 4 + 2; j++){
+      if(matrix[i][j] >= 0.0){
+        printf("  %.5f", matrix[i][j]);
+      }else{
+        printf(" %.5f", matrix[i][j]);
+      }
+    }
+    printf("\n");
+  }
+
+
+  printf("    row = %d ; column = %d \n" , row_number, column_number);
+
   double* row_to_use    = matrix[column_number];
   double* row_to_modify = matrix[row_number];
   int matrix_width = 2 * pgm_info->width + 2;
 
   // Coefficient needed to multiply the row_to_use to make a zero; ie: F2 - alpha*F1.
-  double coefficient = -1 * (row_to_modify[column_number] / row_to_use[column_number]);
+  printf("    ROWCOLUMN = %.5lf ;\n" , row_to_use[4]);
 
-  for(int i = column_number; i < matrix_width - 1; i++){
+  //desde la diagonal (o sea el elemento del medio de la matriz)
+  double coefficient = -1 * (row_to_modify[index] / row_to_use[pgm_info->width]);
+  printf("    coeff = %lf \n" , coefficient);
+
+
+  for(int i = pgm_info->width; i < matrix_width - 1; i++){
     row_to_modify[index++] += coefficient * row_to_use[i];
   }
 
   // Pixel.
   row_to_modify[matrix_width - 1] += coefficient * row_to_use[matrix_width - 1];
   // row_to_modify[index] = 0.0;
+
+  for(int i = 0; i < 16; i++){
+    for(int j = 0; j < 2 * 4 + 2; j++){
+      if(matrix[i][j] >= 0.0){
+        printf("  %.5f", matrix[i][j]);
+      }else{
+        printf(" %.5f", matrix[i][j]);
+      }
+    }
+    printf("\n");
+  }
+
 }
-// 
+//
 // void solve_equations(Matrix* matrix, double* results){
 //   Matrix::reverse_iterator row; // first: row number, second: Row
 //   for(row = matrix->rbegin(); row != matrix->rend(); row++){
