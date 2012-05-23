ad163ed50f530c7ad1a981428d69ef11ddad5582
Alejandro Mataloni <amataloni@gmail.com>
2012-05-22 22:24:25 -0300

    adding prints to know where the program is

--- tp2/tp2.cpp
+++ tp2/tp2.cpp
@@ -30,11 +30,16 @@ int main(int argc, char* argv[]){
   sprintf(output, "%s%s_l%.1f_f%d.pgm", output, nameWithoutExtension.c_str(), lambda, factor);
 
   PGMInfo pgm_info = parse_pgm(picture,factor);
+  printf("Generadno matriz... \n");
   Matrix matrix = build_matrix(lambda, &pgm_info);
+  printf("Termino generacion... \n");
   double results[pgm_info.fHeight * pgm_info.fWidth];
 
+  printf("Empiezo gauss ... \n");
   gauss(matrix, &pgm_info);
   solve_equations(matrix, &pgm_info, results);
+
+  printf("TERMINE gauss ... \n");
   create_new_picture(results, output, &pgm_info);
 
   free_pixels_memory(&pgm_info);
